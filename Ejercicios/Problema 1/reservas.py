1e0e2dad73a25909cea0442d4f5b1295bb41879e
Santiago Vera G <santivera111@gmail.com>
2024-09-13 12:41:45 -0500

    Sigo trabajando

--- Ejercicios/Problema 1/reservas.py
+++ Ejercicios/Problema 1/reservas.py
@@ -1,12 +1,16 @@
 from os import system
+import datetime
 
 def Respuesta_Destino(Ciudad_Destino, Posibles_Ciudades):
-    Respuesta_1 = Ciudad_Destino
-    Ciudad_Destino = Ciudad_Destino.upper()
-    if Ciudad_Destino in Posibles_Ciudades:
-        print(f"Muy bien, has elegido {Respuesta_1}.")
-    else:
-        print(f"{Respuesta_1} no es un respuesta válida. Intente de nuevo.")
+    while True:
+        Respuesta_1 = Ciudad_Destino
+        Ciudad_Destino = Ciudad_Destino.upper()
+        if Ciudad_Destino in Posibles_Ciudades:
+            print(f"Muy bien, has elegido {Respuesta_1}.")
+            break
+        else:
+            print(f"{Respuesta_1} no es un respuesta válida.")
+            Ciudad_Destino = str(input(f"Elije una de las ciudades a las cuales podemos llevarte {Posibles_Ciudades}: "))
     
     return Respuesta_1
     
@@ -16,13 +20,13 @@ def main():
 
     # Primero conoceremos al cliente y generaremos un saludo personalizado.
 
-    Genero = str(input("¿Cómo le gustaría que nos dirigiéramos hacía usted?: Señor o Señora: "))
-    Nombre = str(input("Por favor, ingrese su nombre: "))
-    Apellido = str(input("Por favor, ingrese su apellido: "))
+    Genero = str(input("¿Cómo te gustaría que nos dirigiéramos hacía ti?: Señor o Señora: "))
+    Nombre = str(input("Por favor, ingresa tu nombre: "))
+    Apellido = str(input("Por favor, ingresa tu apellido: "))
 
     system("cls")
 
-    print(f"{Genero}, {Nombre} {Apellido} ¡estamos muy contentos de servirle en TortugüitaFly!")
+    print(f"{Genero}, {Nombre} {Apellido} ¡estamos muy contentos de servirte en TortugüitaFly!")
     
     # Ahora, pediremos al cliente seleccionar su ciudad de origen y destiino.
 
@@ -30,8 +34,8 @@ def main():
 
     while True:
 
-        print("¡Por favor, no use tildes en ninguna de sus respuestas!")
-        Ciudad_Origen = str(input("Indique su ciudad de origen: Medellín, Bogotá o Cartagena: "))
+        print("¡Por favor, no uses tildes en ninguna de tus respuestas!")
+        Ciudad_Origen = str(input("Indica tu ciudad de origen: Medellin, Bogota o Cartagena: "))
 
         system("cls")
 
@@ -42,10 +46,11 @@ def main():
             print(f"Muy bien, has elegido {Respuesta}.")
             break
         else:
-            print(f"{Respuesta} no es una respuesta válida. Intente de nuevo.")
+            print(f"Lamentablemente no volamos desde {Respuesta}. Intenta de nuevo.")
 
     while True:
-        print("Ahora indique su ciudad de destino: ")
+        system("cls")
+        print("Ahora indica tu ciudad de destino: ")
 
         if Ciudad_Origen == "MEDELLIN":
             Ciudad_Destino = str(input("Bogota o Cartagena: "))
@@ -62,7 +67,17 @@ def main():
             Posibles_Ciudades_Destino = ["MEDELLIN", "BOGOTA"]
             Respuesta_Destino(Ciudad_Destino, Posibles_Ciudades_Destino)
             break
+    
+    # Preguntaremos al cliente qué día quiere viajar
 
+    while True:
+        Fecha_Ingresada= input("Indica a fecha en la que quieres viajar (dd/mm/aaaa): ")
+        Fecha_Vuelo = datetime.datetime.strptime(Fecha_Ingresada, "%d/%m/%Y")
+        if Fecha_Vuelo < datetime.now():
+            print("En TortugüitaFly ofrecemos un excelente servicio, pero en este momento no podemos llevarte al pasado. Por Favor, selecciona una fecha válida")
+    
+            
+    
     
 
 if __name__ == "__main__":
