7693047f985150f998aebbfc752ac84f60568cb3
DanieLudens_Biblioteca_UPB <66543657+DanielZafiro@users.noreply.github.com>
2024-09-13 15:27:25 -0500

    Realicé código Satelite.py

--- Ejercicios/Problema 2/satelite.py
+++ Ejercicios/Problema 2/satelite.py
@@ -1,8 +1,35 @@
+from os import system
 
 def main():
-    #Tu código va aquí
-    pass # borra esta línea cuando con inicies tu código
 
+    # Comenzaremos registrando la informcaión de partida del sátelite 
+
+    Altitud_Inicial = int(input("Por favor, ingrese la altitud inicial del satélite en km: "))
+    system("cls")
+    Coeficiente_Arrastre = float(input("Por favor, ingrese el coeficiente de arrastre (Ej: 0.01): "))
+    system("cls")
+    Altitud_Minima =  int(input("Por favor, ingrese la altitud mínima segura en km: "))
+    system("cls")
+
+    # Simulación de orbitas
+
+    Contador_Orbitas = 0
+    Nueva_Altitud_Inicial = Altitud_Inicial
+
+    while Nueva_Altitud_Inicial > Altitud_Minima:
+        Altitud_Perdida = Nueva_Altitud_Inicial * Coeficiente_Arrastre
+        Nueva_Altitud_Inicial -= Altitud_Perdida
+        Coeficiente_Arrastre += 0.0001
+        Contador_Orbitas += 1
+        Nueva_Altitud = Nueva_Altitud_Inicial
+
+        if Altitud_Perdida < (0.001):
+            break
+
+    if Altitud_Perdida < (0.001):
+        print(f"El satélite se ha estabilizado luego de {Contador_Orbitas} órbitas completadas. La altitud final alcanzada fue de {Nueva_Altitud}.")
+    else:
+        print(f"El stálite ha reingresado a la atmósfera terrestre luego de {Contador_Orbitas} órbitas completadas alcanzando una altitud de {Nueva_Altitud}.")
 
 if __name__ == "__main__":
     main()
